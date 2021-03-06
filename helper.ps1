8a0d21b17e1a23491dcfdfa6fb1143389e583daa
Victor Loh <vic94loh@hotmail.com>
2021-03-06 17:08:05 +0800

    test helper (TODO: provide bash alternative)

---
+++ helper.ps1
@@ -0,0 +1,31 @@
+$TIMESTAMP = (date -u +%Y%m%d.%H%M%S)
+$COVER_PROFILE_FILENAME = "logfeller-testcov"
+
+$target = $args[0]
+
+switch ($target) {
+    generate {
+        go generate ./...
+    }
+    test {
+        go generate ./...
+        $packages = If ($args[1] -eq $null) { "./..." } else { $args[1] }
+        go test -cover $packages
+    }
+    showtestcov {
+        go generate ./...
+        $packages = If ($args[1] -eq $null) { "./..." } else { $args[1] }
+        $fullcoverpath = "$env:TEMP\$COVER_PROFILE_FILENAME.$TIMESTAMP.out"
+        Write-Output "Running tests over $packages and saving output to $fullcoverpath"
+        go test $packages -covermode count -coverprofile $fullcoverpath
+        go tool cover -html="$fullcoverpath"
+    }
+    lint {
+        go generate ./...
+        golangci-lint --version
+        golangci-lint run
+    }
+    Default {
+        Write-Output "usage: powershell .\helper.ps1 (generate|test|showtestcov|lint)"
+    }
+}
